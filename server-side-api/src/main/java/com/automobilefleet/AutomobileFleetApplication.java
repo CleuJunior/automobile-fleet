f42a0ec68744f8f835021ae544431812004ff0f8
Cleonildo Jr <cleu.junior@gmail.com>
2022-08-18 21:38:04 -0300

    First Commit

---
+++ server-side-api/src/main/java/com/automobilefleet/AutomobileFleetApplication.java
@@ -0,0 +1,13 @@
+package com.automobilefleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileFleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobileFleetApplication.class, args);
+	}
+
+}
