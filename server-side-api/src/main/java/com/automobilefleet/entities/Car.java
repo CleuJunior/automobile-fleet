771f4e8bfb4681413d2f79fb3ab9dba23a3ce644
Cleonildo Jr <cleu.junior@gmail.com>
2022-08-22 23:25:44 -0300

    Car entity

---
+++ server-side-api/src/main/java/com/automobilefleet/entities/Car.java
@@ -0,0 +1,67 @@
+package com.automobilefleet.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Table(name = "car_entity")
+@Entity
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Car implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "_id", nullable = false)
+    @Getter
+    private Long id;
+
+    @Column(name = "car_name", unique = true, nullable = false)
+    @Getter
+    @Setter
+    private String name;
+
+    @Column(name = "car_description", length = 30, nullable = false)
+    @Getter
+    @Setter
+    private String description;
+
+    @Column(name = "daily_rate", nullable = false)
+    @Getter
+    @Setter
+    private Double dailyRate;
+
+    @Column(name = "car_avaliable", nullable = false)
+    @Getter
+    @Setter
+    private Boolean avaliable;
+
+    @Column(name = "license_plate", length = 30, nullable = false)
+    @Getter
+    @Setter
+    private String licensePlate;
+
+    @Column(name = "brand_id", nullable = false)
+    @Getter
+    @Setter
+    private Brand brandId;
+
+    @Column(name = "created_at", nullable = false)
+    @Getter
+    private LocalDateTime createdAt;
+
+    public Car() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
